3d58aaae5f3102a38f50728f123a6a7a39eb1ee4
Nick Jenkins <117663799+nickj1976@users.noreply.github.com>
2024-12-13 13:04:16 +0000

    Add files via upload

---
+++ install-intune/install-chrome.sh
@@ -0,0 +1,3 @@
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | tee /etc/apt/sources.list.d/google-chrome.list
+apt update && apt install -y microsoft-edge-stable
