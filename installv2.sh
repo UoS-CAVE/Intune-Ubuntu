3ebcf429b2a9f9784e79a41cc91b9e9bf926787e
nickj1976 <117663799+nickj1976@users.noreply.github.com>
2024-12-20 11:18:44 +0000

    deleted

--- installv2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-echo "installing requirements..."
-apt update
-apt-get install -y git
-echo "cloning repo..."
-git clone https://github.com/UoS-CAVE/Intune-Ubuntu.git
-cd ./Intune-Ubuntu/install-intune
-./install-intune-full.sh
